3ffd431fc5e9e71c8c7154181fd3cdb9287c188a
dannybaque <baquedanny9@gmail.com>
2026-04-29 22:49:07 -0500

    chore: add timeout config to playwright.config.ts

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -7,3 +7,9 @@ test('should navigate to Docs section when clicking Docs link',async({page})=>{
 
   await expect(page).toHaveURL(/\/docs\/intro/);
 });
+
+test('should expect heading to be visible', async({page}) =>{
+    await page.goto('https://playwright.dev');
+
+    await expect(page.getByRole('heading',{name:'Playwright enables reliable web automation for testing, scripting, and AI agents.'})).toBeVisible();
+});
