19a542bd2562157c4ea4d008a77c4dd0026973f6
dannybaque <baquedanny9@gmail.com>
2026-05-07 00:44:54 -0500

    chore: add list reporter alongside html

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+  ['html'],
+  ['list'],]
+  ,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 30000,
   expect: { timeout: 5000 },
